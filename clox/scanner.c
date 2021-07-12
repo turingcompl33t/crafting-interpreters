0b6e6226cf2bfa9457351dc915f933e90723a133
turingcompl33t <kdotterrer@gmail.com>
2021-07-11 22:23:35 -0400

    fix bug in scanner with nil keyword

--- clox/scanner.c
+++ clox/scanner.c
@@ -215,7 +215,7 @@ static TokenType identifierType() {
         }
       }
     case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
-    case 'n': return checkKeyword(1, 2, "nil", TOKEN_NIL);
+    case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
     case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
     case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
     case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
